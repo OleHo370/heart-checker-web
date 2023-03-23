cae95040aa87b9353f1713537affd1c8e97a710f
OleHo370 <127061964+OleHo370@users.noreply.github.com>
2023-03-23 14:10:00 -0700

    1.0.1

---
+++ index.js
@@ -0,0 +1,29 @@
+// Parse the URL to extract the id_token parameter
+(async () => {
+	const log = console.log;
+
+	let user;
+	let idToken = localStorage.getItem('idToken');
+
+	if (idToken && idToken != 'null') user = jwt_decode(idToken);
+	else idToken = null;
+
+	if (!idToken || user.exp < Date.now() / 1000) {
+		// get the AWS Cognito id_token from the URL
+		let params = location.search;
+		if (!params) params = '?' + location.hash.slice(1);
+		const urlParams = new URLSearchParams(params);
+		idToken = urlParams.get('id_token');
+		localStorage.setItem('idToken', idToken);
+		if (!idToken) return;
+		user = jwt_decode(idToken);
+
+		// hide the token from the URL
+		window.history.pushState(null, '', location.href.split(/[?#]/)[0]);
+	}
+
+	// DELETE LATER
+	window.idToken = idToken; // for debugging
+
+	log(user);
+})();
